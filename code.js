ee2712123ca3294ba804cc209fa964c320056366
root <root@LucasUNPAZ.unpaz.edu.ar>
2020-02-19 12:04:47 -0300

    Se agrego una visualización de "Cargando" mientras se realiza la petición del JSON a Google sheets. Se modificó la funcionalidad para que solo se visualicen las respuestas una a la vez. Se estructuró un poco el código en funciones.

--- code.js
+++ code.js
@@ -4,10 +4,10 @@ fetch(baseURL)
         return response.json();
     })
     .then(function (myJson) {
-        var faqDiv = document.getElementById("faq-campus-unpaz");
+        let faqDiv = document.getElementById("faq-campus-unpaz");
 
-        var questions = [];
-        var answers = [];
+        let questions = [];
+        let answers = [];
 
         myJson.feed.entry.forEach(element => {
             if (element.title.$t.startsWith("A") && element.gs$cell.row > 1)
@@ -17,35 +17,61 @@ fetch(baseURL)
         });
 
         if (questions.length == answers.length) {
-            for (let index = 0; index < questions.length; index++) {
-                var questionContainer = document.createElement("div");
-                questionContainer.setAttribute("style", "margin-bottom:1%;");
-
-                var questionBtn = document.createElement("button");
-                questionBtn.classList.add("lfrias-faq-collapsible");
-                questionBtn.addEventListener("click", function () {
-                    this.classList.toggle("lfrias-faq-active");
-                    var content = this.nextElementSibling;
-                    if (content.style.maxHeight) {
-                        content.style.maxHeight = null;
-                    } else {
-                        content.style.maxHeight = content.scrollHeight + "px";
-                    }
-                });
-                questionBtn.appendChild(document.createTextNode(questions[index]))
-
-                var answerDiv = document.createElement("div");
-                answerDiv.classList.add("lfrias-faq-content");
-                var answerText = document.createElement("p");
-                answerText.innerHTML = answers[index]
-                answerDiv.appendChild(answerText);
-
-
-                questionContainer.appendChild(questionBtn);
-                questionContainer.appendChild(answerDiv);
-
-                faqDiv.appendChild(questionContainer);
-
-            }
+            faqDiv.classList.remove("lfrias-preloader");
+            for (let i = 0; i < questions.length; i++)
+                faqDiv.appendChild(createFaqDiv(questions[i], answers[i]));
         }
     });
+
+function createFaqDiv(question, answer) {
+    let questionContainer = document.createElement("div");
+    questionContainer.setAttribute("style", "margin-bottom:10px;");
+
+    let questionBtn = document.createElement("button");
+    questionBtn.classList.add("lfrias-faq-collapsible");
+    questionBtn.addEventListener("click", function () {
+        if (!isVisible(this.nextElementSibling) && !this.classList.contains("lfrias-faq-active")) {
+            let activeQuestionsBtns = document.getElementsByClassName("lfrias-faq-active");
+
+            if (activeQuestionsBtns.length > 0) {
+                for (let index = 0; index < activeQuestionsBtns.length; index++) {
+                    hideElement(activeQuestionsBtns.item(index).nextElementSibling);
+                    activeQuestionsBtns.item(index).classList.remove("lfrias-faq-active");
+                }
+            }
+            showElement(this.nextElementSibling);
+            this.classList.add("lfrias-faq-active");
+        }else{
+            hideElement(this.nextElementSibling);
+            this.classList.remove("lfrias-faq-active");
+        }
+
+    });
+    questionBtn.appendChild(document.createTextNode(question))
+
+    let answerDiv = document.createElement("div");
+    answerDiv.classList.add("lfrias-faq-content");
+    let answerText = document.createElement("p");
+    answerText.innerHTML = answer;
+    answerDiv.appendChild(answerText);
+
+
+    questionContainer.appendChild(questionBtn);
+    questionContainer.appendChild(answerDiv);
+
+    return questionContainer;
+}
+
+function hideElement(element) {
+    if (element.style.maxHeight) {
+        element.style.maxHeight = null;
+    }
+}
+
+function showElement(element) {
+    element.style.maxHeight = element.scrollHeight + "px";
+}
+
+function isVisible(element) {
+    return element.style.maxHeight
+}
